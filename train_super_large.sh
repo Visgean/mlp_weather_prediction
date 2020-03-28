45a38250bf565563fa6b107f828606d6c0274adf
Martin <me@visgean.me>
2020-03-28 19:28:30 +0000

    make super large model long

--- train_super_large.sh
+++ train_super_large.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 #SBATCH -N 1	  # nodes requested
 #SBATCH -n 1	  # tasks requested
-#SBATCH --partition=Teach-Standard
+#SBATCH --partition=Teach-LongJobs
 #SBATCH --gres=gpu:4
 #SBATCH --mem=24000  # memory in Mb
-#SBATCH --time=0-8:00:00
+#SBATCH --time=0-24:00:00
 #SBATCH -c 4
 
 export CUDA_HOME=/opt/cuda-9.0.176.1/
